f547767f65cd146390ac9c9c5fc1cbf93187bba7
yujonglee <yujonglee@g.skku.edu>
2021-04-30 09:11:52 +0900

    style: change filename .js to .jsx. and use parameterized test

--- src/component/LoginPage/Intro/Intro.test.jsx
+++ src/component/LoginPage/Intro/Intro.test.jsx
@@ -5,20 +5,22 @@ import Intro from './Intro'
 import { MemoryRouter } from 'react-router-dom'
 
 describe('Intro', () => {
-    it('render Description text', () => {
+    it.each([
+        'stolage',
+        'Storage에 label을 입히다',
+        'stolage는 파일을 단순한 디렉토리로 저장하지않습니다.',
+        '파일을 label로 묘사하세요.'
+    ])
+    ('render Description text', (description) => {
         const { container } = render(<Intro />, {wrapper: MemoryRouter})
-
-        expect(container).toHaveTextContent('stolage');
-        expect(container).toHaveTextContent('Storage에 label을 입히다');
-        expect(container).toHaveTextContent('stolage는 파일을 단순한 디렉토리로 저장하지않습니다.');
-        expect(container).toHaveTextContent('파일을 label로 묘사하세요.');
+        expect(container).toHaveTextContent(description);
     })
 
     it('render Button', () => {
         const { getByTestId } = render(<Intro />, {wrapper: MemoryRouter})
         const button = getByTestId('btn');
         
-        expect(button).toBeInTheDocument();
         expect(button).toHaveTextContent('체험하기');
+        //나중에 데모페이지 만들어지만 클릭했을 때에 대한 테스트도 작성하면 좋을 듯
     })
 })
