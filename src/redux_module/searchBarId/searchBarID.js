da551d9b2db18e3d1f4a98c003ba107c4a6dc20e
yujonglee <yujonglee@g.skku.edu>
2021-05-05 18:59:21 +0900

    searchBarID모듈을 redux-toolkit을 이용해 리팩토링하라

--- src/redux_module/searchBarId/searchBarID.js
+++ src/redux_module/searchBarId/searchBarID.js
@@ -1,67 +1,37 @@
+/* eslint-disable no-param-reassign */
+import { createSlice } from '@reduxjs/toolkit';
 import _ from 'lodash';
 
-export const addNewId = () => ({ type: 'addNewId' });
-export const deleteID = (id) => (
-  {
-    type: 'deleteID',
-    payload: {
-      id,
-    },
-  }
-);
-export const changeCurrentId = (id) => (
-  {
-    type: 'changeCurrentId',
-    payload: {
-      id,
-    },
-  }
-);
-
-// -1은 Group SearchBar, 0부터는 Tag SearchBar
 const initialState = {
-  searchBarID: {
-    current: 0,
-    last: 0,
-    all: [-1, 0],
-  },
+  current: 0,
+  last: 0,
+  all: [-1, 0],
 };
 
-export default function searchBarIDReducer(state = initialState, action) {
-  switch (action.type) {
-  case ('addNewId'):
-    const newID = state.searchBarID.last + 1;
-    return {
-      searchBarID: {
-        ...state.searchBarID,
-        current: newID,
-        last: newID,
-        all: [...state.searchBarID.all, newID],
-      },
-    };
+const searchBarIDSlice = createSlice({
+  name: 'searchBarID',
+  initialState,
+  reducers: {
+    addNewID: (state, action) => {
+      const newID = state.last + 1;
+      state.current = newID;
+      state.last = newID;
+      state.all.push(newID);
+    },
+    deleteID: (state, action) => {
+      const newLast = (state.last === action.payload)
+        ? _.nth(state.all, -2)
+        : state.last;
 
-  case ('deleteID'):
-    const newLast = (state.searchBarID.last === action.payload.id)
-      ? _.nth(state.searchBarID.all, -2)
-      : state.searchBarID.last;
-    return {
-      searchBarID: {
-        ...state.searchBarID,
-        current: 0,
-        last: newLast,
-        all: _.without(state.searchBarID.all, action.payload.id),
-      },
-    };
+      state.current = 0;
+      state.last = newLast;
+      state.all = _.without(state.all, action.payload);
+    },
+    changeCurrentID: (state, action) => {
+      state.current = action.payload;
+    },
+  },
+});
 
-  case ('changeCurrentId'):
-    return {
-      searchBarID: {
-        ...state.searchBarID,
-        current: action.payload.id,
-      },
-    };
-
-  default:
-    return {};
-  }
-}
+export const { addNewID, deleteID, changeCurrentID } = searchBarIDSlice.actions;
+export default searchBarIDSlice.reducer;
--- src/redux_module/searchBarId/searchBarID.test.js
+++ src/redux_module/searchBarId/searchBarID.test.js
@@ -1,61 +1,54 @@
-import searchBarIDReducer, { addNewId, deleteID, changeCurrentId } from './searchBarID';
+import searchBarIDReducer,
+{
+  addNewID,
+  deleteID,
+  changeCurrentID,
+} from './searchBarID';
 
 describe('module: searchBarId', () => {
   describe('reducer: setSearchBarId', () => {
     it('add new searchBarId', () => {
       const initialState = {
-        searchBarID: {
-          current: 2,
-          last: 2,
-          all: [-1, 0, 2],
-        },
+        current: 2,
+        last: 2,
+        all: [-1, 0, 2],
       };
 
-      const state = searchBarIDReducer(initialState, addNewId());
+      const state = searchBarIDReducer(initialState, addNewID());
       expect(state).toEqual({
-        searchBarID: {
-          current: 3,
-          last: 3,
-          all: [-1, 0, 2, 3],
-        },
+        current: 3,
+        last: 3,
+        all: [-1, 0, 2, 3],
       });
     });
 
     it('delete existing searchBarID', () => {
       const initialState = {
-        searchBarID: {
-          current: 1,
-          last: 2,
-          all: [-1, 0, 1, 2],
-        },
+        current: 1,
+        last: 2,
+        all: [-1, 0, 1, 2],
       };
 
       const state = searchBarIDReducer(initialState, deleteID(2));
       expect(state).toEqual({
-        searchBarID: {
-          current: 0,
-          last: 1,
-          all: [-1, 0, 1],
-        },
+        current: 0,
+        last: 1,
+        all: [-1, 0, 1],
       });
     });
 
     it('change current searchBarID', () => {
       const initialState = {
-        searchBarID: {
-          current: 1,
-          last: 2,
-          all: [-1, 0, 1, 2],
-        },
+        current: 1,
+        last: 2,
+        all: [-1, 0, 1, 2],
       };
 
-      const state = searchBarIDReducer(initialState, changeCurrentId(2));
+      const state = searchBarIDReducer(initialState, changeCurrentID(2));
       expect(state).toEqual({
-        searchBarID: {
-          current: 2,
-          last: 2,
-          all: [-1, 0, 1, 2],
-        },
+        current: 2,
+        last: 2,
+        all: [-1, 0, 1, 2],
       });
     });
   });
