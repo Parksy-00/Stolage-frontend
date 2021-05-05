c6da8b6d002d7f3e08ac833d30a984bcf813288d
yujonglee <yujonglee@g.skku.edu>
2021-05-05 19:04:16 +0900

    selected모듈의 불필요한 액션생성함수 테스트를 삭제하라

--- src/redux_module/selected/selected.test.js
+++ src/redux_module/selected/selected.test.js
@@ -1,18 +1,6 @@
 import selectedReducer, { updateSelected } from './selected';
 
 describe('module: selected', () => {
-  it('action : updateSelected', () => {
-    const exp = {
-      type: 'updateSelected',
-      payload: {
-        id: 2,
-        newSelected: ['this', 'that'],
-      },
-    };
-
-    expect(updateSelected(2, ['this', 'that'])).toEqual(exp);
-  });
-
   it('reducer: setSelected', () => {
     const initialState = {
       selected: {
