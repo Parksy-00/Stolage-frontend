d100efafcab56b4a36ac1582023388adc2d2ac55
Parksy-00 <63638539+Parksy-00@users.noreply.github.com>
2021-05-07 20:35:13 +0900

    getTagList selector를 추가하라

--- src/redux_module/_selectors_/selectors.test.js
+++ src/redux_module/_selectors_/selectors.test.js
@@ -1,4 +1,4 @@
-import { getFileList } from './selectors';
+import { getFileList, getTagList } from './selectors';
 
 describe('Test Selector', () => {
   it('getFileList', () => {
@@ -23,12 +23,39 @@ describe('Test Selector', () => {
         },
       },
     };
-    const files = getFileList(state);
+    const fileList = getFileList(state);
 
-    expect(files).toEqual({
+    expect(fileList).toEqual({
       1: { id: 1, name: '20180620', tags: ['2018년', '6월', '20번'] },
       2: { id: 2, name: '20180321', tags: ['2018년', '3월', '21번', '킬러'] },
       3: { id: 3, name: '20181130', tags: ['2018년', '11월', '30번', '킬러'] },
+    });
+  });
+
+  it('getTagList', () => {
+    const state = {
+      matchedItems: {
+        tags: {
+          error: null,
+          items: {
+            1: { id: 1, name: '6월', group: ['짝수', '평가원', '달'] },
+            2: { id: 2, name: '2016년', group: ['년', '짝수'] },
+            3: { id: 3, name: '21번', group: ['번호', '홀수'] },
+          },
+        },
+        files: {
+          error: null,
+          items: {},
+        },
+      },
+    };
+
+    const tagList = getTagList(state);
+
+    expect(tagList).toEqual({
+      1: { id: 1, name: '6월', group: ['짝수', '평가원', '달'] },
+      2: { id: 2, name: '2016년', group: ['년', '짝수'] },
+      3: { id: 3, name: '21번', group: ['번호', '홀수'] },
     });
   });
 });
