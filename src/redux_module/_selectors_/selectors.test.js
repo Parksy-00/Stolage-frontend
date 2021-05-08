8f7d93e6ba2f3ae2a8d2e4d3db954e9eb942dca4
Parksy-00 <63638539+Parksy-00@users.noreply.github.com>
2021-05-08 14:59:23 +0900

    matchedItems모듈에 fixture를 도입하라

--- src/redux_module/_selectors_/selectors.test.js
+++ src/redux_module/_selectors_/selectors.test.js
@@ -1,61 +1,51 @@
-import { getFileList, getTagList } from './selectors';
+import { getFileList, getTagList, getRelatedTags } from './selectors';
+import matchedItems from '../matchedItems/matchedItems.fixture';
 
 describe('Test Selector', () => {
   it('getFileList', () => {
-    const state = {
-      matchedItems: {
-        tags: {
-          error: null,
-          items: [],
-        },
-        files: {
-          error: null,
-          items: {
-            0: {
-              1: { id: 1, name: '20180620', tags: ['2018년', '6월', '20번'] },
-              2: { id: 2, name: '20180321', tags: ['2018년', '3월', '21번', '킬러'] },
-            },
-            1: {
-              1: { id: 1, name: '20180620', tags: ['2018년', '6월', '20번'] },
-              3: { id: 3, name: '20181130', tags: ['2018년', '11월', '30번', '킬러'] },
-            },
-          },
-        },
-      },
-    };
+    const state = { matchedItems };
     const fileList = getFileList(state);
 
     expect(fileList).toEqual({
-      1: { id: 1, name: '20180620', tags: ['2018년', '6월', '20번'] },
-      2: { id: 2, name: '20180321', tags: ['2018년', '3월', '21번', '킬러'] },
-      3: { id: 3, name: '20181130', tags: ['2018년', '11월', '30번', '킬러'] },
+      1: {
+        id: 1,
+        name: '20180620',
+        tags: {
+          1: { id: 1, name: '2018년', group: ['짝수', '년'] },
+          2: { id: 2, name: '6월', group: ['짝수', '월', '평가원'] },
+          3: { id: 3, name: '20번', group: ['짝수', '번호'] },
+        },
+      },
+      2: {
+        id: 2,
+        name: '20200321',
+        tags: {
+          4: { id: 4, name: '2020년', group: ['짝수', '년'] },
+          5: { id: 5, name: '3월', group: ['홀수', '월', '교육청'] },
+          6: { id: 6, name: '21번', group: ['홀수', '번호'] },
+        },
+      },
+      3: {
+        id: 3,
+        name: '20181130',
+        tags: {
+          1: { id: 1, name: '2018년', group: ['짝수', '년'] },
+          7: { id: 7, name: '11월', group: ['홀수', '월', '평가원', '수능'] },
+          8: { id: 8, name: '30번', group: ['짝수', '번호'] },
+        },
+      },
     });
   });
 
   it('getTagList', () => {
-    const state = {
-      matchedItems: {
-        tags: {
-          error: null,
-          items: {
-            1: { id: 1, name: '6월', group: ['짝수', '평가원', '달'] },
-            2: { id: 2, name: '2016년', group: ['년', '짝수'] },
-            3: { id: 3, name: '21번', group: ['번호', '홀수'] },
-          },
-        },
-        files: {
-          error: null,
-          items: {},
-        },
-      },
-    };
+    const state = { matchedItems };
 
     const tagList = getTagList(state);
 
     expect(tagList).toEqual({
       1: { id: 1, name: '6월', group: ['짝수', '평가원', '달'] },
       2: { id: 2, name: '2016년', group: ['년', '짝수'] },
-      3: { id: 3, name: '21번', group: ['번호', '홀수'] },
+      3: { id: 3, name: '20번', group: ['번호', '짝수'] },
     });
   });
 });
