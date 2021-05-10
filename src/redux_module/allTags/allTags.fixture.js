e0d82e9e76b47e0d763c179621b89a640b43b707
Parksy-00 <63638539+Parksy-00@users.noreply.github.com>
2021-05-10 16:50:54 +0900

    allTags.fixture를 추가하라

---
+++ src/redux_module/allTags/allTags.fixture.js
@@ -0,0 +1,13 @@
+const tags = [
+  { id: 1, name: '2018년', group: ['짝수', '년'] },
+  { id: 2, name: '6월', group: ['짝수', '월', '평가원'] },
+  { id: 3, name: '20번', group: ['짝수', '번호'] },
+  { id: 4, name: '2020년', group: ['짝수', '년'] },
+  { id: 5, name: '3월', group: ['홀수', '월', '교육청'] },
+  { id: 6, name: '21번', group: ['홀수', '번호'] },
+  { id: 7, name: '11월', group: ['홀수', '월', '평가원', '수능'] },
+  { id: 8, name: '30번', group: ['짝수', '번호'] },
+];
+
+export { tags };
+export default {};
